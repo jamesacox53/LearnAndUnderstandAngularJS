55fc3ed9902c2b1f198d64b39d1257d9a6becfc3
jamesacox53 <jamesacox53@gmail.com>
2024-06-20 14:03:54 +0100

    Completed Modules, Apps, and Controllers

---
+++ 02-ModelViewWhatever/04-ModulesAppsAndControllers/app.js
@@ -0,0 +1,10 @@
+// MODULE
+var angularApp = angular.module('angularApp', []);
+
+// CONTROLLERS
+angularApp.controller('mainController', ['$scope', function ($scope) {
+    // In here will be the model
+}]);
+
+
+
