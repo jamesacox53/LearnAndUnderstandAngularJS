22e808787a5770266839c3186e5292e5ae989784
jamesacox53 <jamesacox53@gmail.com>
2024-06-21 11:05:18 +0100

    Completed Javascript Aside: Dependency Injection

---
+++ 03-ServicesAndDependencyInjection/01-DependencyInjection/app.js
@@ -0,0 +1,11 @@
+var Person = function (firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName; 
+}
+
+function logPerson(person) {
+    console.log(john);
+}
+
+var john = new Person('John', 'Doe');
+logPerson(john);
