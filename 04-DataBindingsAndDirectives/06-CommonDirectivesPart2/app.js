6a6e2be6be06bbacc7900b08519d8d4e13c64f65
jamesacox53 <jamesacox53@gmail.com>
2024-06-28 15:50:46 +0100

    Completed Common Directives (Part 2)

---
+++ 04-DataBindingsAndDirectives/06-CommonDirectivesPart2/app.js
@@ -0,0 +1,9 @@
+var myApp = angular.module('myApp', []);
+
+myApp.controller('mainController', ['$scope', '$filter', function($scope, $filter) {
+    $scope.alertClick = function() {
+        alert("Clicked!");
+    }
+
+    $scope.name = "John Doe";
+}]);
