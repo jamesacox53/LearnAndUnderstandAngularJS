30e811bdd8fd01d8eaee4c632e448a3b8bb5e500
jamesacox53 <jamesacox53@gmail.com>
2024-07-08 15:44:08 +0100

    Completed Binding Data (Part 2)

---
+++ 08-BuildAnApp/04-BindingDataPart2/app.js
@@ -0,0 +1,83 @@
+import apiKey from '../apiKey.js';
+
+// MODULE
+var weatherApp = angular.module('weatherApp', ['ngRoute', 'ngResource']);
+
+// ROUTES
+weatherApp.config(function ($routeProvider) {
+   
+    $routeProvider
+    
+    .when('/', {
+        templateUrl: 'pages/home.htm',
+        controller: 'homeController'
+    })
+    
+    .when('/forecast', {
+        templateUrl: 'pages/forecast.htm',
+        controller: 'forecastController'
+    })
+    .when('/forecast/:days', {
+        templateUrl: 'pages/forecast.htm',
+        controller: 'forecastController'
+    })
+    
+});
+
+// SERVICES
+weatherApp.service('cityService', function() {
+   
+    this.city = "London";
+    
+});
+
+// CONTROLLERS
+weatherApp.controller('homeController', ['$scope', 'cityService', function($scope, cityService) {
+    
+    $scope.city = cityService.city;
+    
+    $scope.$watch('city', function() {
+       cityService.city = $scope.city; 
+    });
+    
+}]);
+
+weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParams', 'cityService', function($scope, $resource, $routeParams, cityService) {
+    function getWeatherResults(city, days, apiKey) {
+        const daysInt = parseInt(days, 10);
+        
+        const weatherAPI = $resource('https://api.openweathermap.org/data/2.5/forecast',
+            { callback: "JSON_CALLBACK" }, { get: { method: "JSONP" }});
+        
+        const endDay = new Date();
+        endDay.setDate(endDay.getDate() + daysInt);
+        endDay.setHours(0, 0, 0, 0);
+
+        return weatherAPI.get({ q: city, appid: apiKey })
+        .$promise.then(function(weatherResults) {
+            const weatherArr = weatherResults.list;
+
+            return weatherArr.filter(weatherObj => (new Date(weatherObj.dt * 1000)) < endDay);
+        });
+    };
+    
+    $scope.city = cityService.city;
+    $scope.days = $routeParams.days || 2;
+    
+    $scope.getWeatherResults = function() {
+        getWeatherResults($scope.city, $scope.days, apiKey)
+        .then(function(weatherArr) {
+            $scope.weatherArr = weatherArr;
+        });
+    }
+    
+    $scope.convertToFahrenheit = function(degK) {
+        return Math.round((1.8 * (degK - 273)) + 32);
+    }
+
+    $scope.convertToDate = function(dt) {
+        return new Date(dt * 1000);
+    }
+
+    $scope.getWeatherResults();
+}]);
