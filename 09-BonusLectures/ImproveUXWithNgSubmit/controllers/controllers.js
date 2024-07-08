1c21547efa4073df65d9adef7c5b79b8ad398636
jamesacox53 <jamesacox53@gmail.com>
2024-07-08 17:11:05 +0100

    Completed Improving our SPA's UX With ng-submit

---
+++ 09-BonusLectures/ImproveUXWithNgSubmit/controllers/controllers.js
@@ -0,0 +1,57 @@
+import weatherApp from '../app.js';
+import apiKey from '../../apiKey.js';
+
+// CONTROLLERS
+weatherApp.controller('homeController', ['$scope', '$location', 'cityService', function($scope, $location, cityService) {
+    $scope.city = cityService.city;
+    
+    $scope.$watch('city', function() {
+       cityService.city = $scope.city; 
+    });
+
+    $scope.submit = function() {
+        $location.path("/forecast");
+    }
+}]);
+
+weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParams', 'cityService', function($scope, $resource, $routeParams, cityService) {
+    function getWeatherResults(city, days, apiKey) {
+        const daysInt = parseInt(days, 10);
+        
+        const weatherAPI = $resource('https://api.openweathermap.org/data/2.5/forecast',
+            { callback: "JSON_CALLBACK" }, { get: { method: "JSONP" }});
+        
+        const endDay = new Date();
+        endDay.setDate(endDay.getDate() + daysInt);
+        endDay.setHours(0, 0, 0, 0);
+
+        return weatherAPI.get({ q: city, appid: apiKey })
+        .$promise.then(function(weatherResults) {
+            const weatherArr = weatherResults.list;
+
+            return weatherArr.filter(weatherObj => (new Date(weatherObj.dt * 1000)) < endDay);
+        });
+    };
+    
+    $scope.city = cityService.city;
+    $scope.days = $routeParams.days || 2;
+    
+    $scope.getWeatherResults = function() {
+        getWeatherResults($scope.city, $scope.days, apiKey)
+        .then(function(weatherArr) {
+            $scope.weatherArr = weatherArr;
+        });
+    }
+    
+    $scope.convertToFahrenheit = function(degK) {
+        return Math.round((1.8 * (degK - 273)) + 32);
+    }
+
+    $scope.convertToDate = function(dt) {
+        return new Date(dt * 1000);
+    }
+
+    $scope.dateFormat = 'MMM d, y HH:mm:ss';
+
+    $scope.getWeatherResults();
+}]);
---
+++ 09-BonusLectures/ImproveUXWithNgSubmit/services/services.js
@@ -0,0 +1,8 @@
+import weatherApp from '../app.js';
+
+// SERVICES
+weatherApp.service('cityService', function() {
+   
+    this.city = "London";
+    
+});
